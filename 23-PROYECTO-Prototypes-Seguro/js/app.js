1ec7aa02ec931c2c398b91f3d743bb0670f11287
andreatrasvina <alucerotrasvina@gmail.com>
2025-05-07 11:41:33 -0700

    174. calculo del seguro

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -6,6 +6,54 @@ function Seguro(marca, year, tipo){
     this.tipo = tipo;
 }
 
+//prototype para realizar cotizacion con los datos
+Seguro.prototype.cotizarSeguro = function(){
+
+    /*
+        1. americano 1.15
+        2. asiatico 1.05
+        3. europeo 1.35
+    */
+
+    let cantidad;
+    let base = 2000;
+
+    switch (this.marca) {
+        case "1":
+            cantidad = base * 1.15;
+            break;
+
+        case "2":
+            cantidad = base * 1.05;
+            break;
+        
+        case "3":
+            cantidad = base * 1.35;
+            break;
+
+        default:
+            break;
+    }
+
+    //leer el año
+    const diferencia = new Date().getFullYear() - this.year;
+
+    //cada año que la diferencia es mayor se reduce el costo un 3%
+    cantidad -= ((diferencia * 3) * cantidad) / 100;
+
+    /**
+     * si el seguro es basico se multiplica por un 30% mas
+     * si el seguro es completo se multiplica por un 50% mas
+     */
+    if(this.tipo === 'basico'){
+        cantidad *= 1.30;
+    }else{
+        cantidad *= 1.50;
+    }
+
+    return cantidad;
+}
+
 //interfaz
 function UI(){}
 
@@ -74,9 +122,14 @@ function cotizarSeguro(e){
 
     if( marca === '' || year === '' || tipo === '' ){
         ui.mostrarMensaje('Todos los campos son necesarios', 'error');
-    }else {
-        ui.mostrarMensaje('Cotizando...', 'correcto');
+        return;
     }
 
-    console.log('cotizando...');
+    ui.mostrarMensaje('Cotizando...', 'correcto');
+
+    //instanciar el seguro
+    const seguro = new Seguro(marca, year, tipo);
+    seguro.cotizarSeguro();
+
+    //utilizar elproto que cotiza
 }
