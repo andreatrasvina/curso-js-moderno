6b8ac8fe39a30b6bc03671c727bbbcb76d3dd549
andreatrasvina <alucerotrasvina@gmail.com>
2025-05-09 22:22:39 -0700

    176. Metodos y metodos estaticos en las clases

--- 24-POO/js/01-app.js
+++ 24-POO/js/01-app.js
@@ -6,11 +6,26 @@ class Cliente {
         this.saldo = saldo;
     }
 
+    //metodo
+    mostrarInformacion(){
+        return `Cliente: ${this.nombre}, tu saldo es de $${this.saldo}`;
+    }
+
+    //metodos estaticos
+    //no tienen acceso a instancias (this, obj, etc)
+    static bienvenida(){
+        return 'Bienvenida al cajero';
+    }
+
 }
 
 //instanciar
 const yeya = new Cliente('yeya', 100000);
+console.log(yeya.mostrarInformacion());
 console.log(yeya);
+
+//es una propiedad de la clase pero no de sus instancias o objetos
+console.log( Cliente.bienvenida() );
 
 
 //class expresion esta nadie la usa ajajajaja
