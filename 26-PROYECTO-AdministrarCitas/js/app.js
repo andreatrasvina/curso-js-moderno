c2741225e7aaa7b4cebc53017af9abfc0b9cdff9
andreatrasvina <alucerotrasvina@gmail.com>
2025-06-17 23:51:40 -0700

    191. creando obj de cita

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -4,3 +4,12 @@ const propietarioInput = document.querySelector('#propietario');
 const emailInput = document.querySelector('#email');
 const fechaInput = document.querySelector('#fecha');
 const sintomasInput = document.querySelector('#sintomas');
+
+//objeto de cita
+const citaObj = {
+    paciente: '',
+    propietario: '',
+    email: '',
+    fecha: '',
+    sintomas: '',
+}
