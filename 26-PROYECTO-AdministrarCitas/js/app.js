881404962668712b301bdc0e61dad30990f2a709
andreatrasvina <alucerotrasvina@gmail.com>
2025-06-18 12:58:14 -0700

    199. notificaciones de error

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -36,7 +36,42 @@ function submitCita(e) {
     e.preventDefault();
 
     if(Object.values(citaObj).some(valor => valor.trim() === "")){
-        console.log('todos los campos son obligatorios');
+        const notificacion = new Notificacion({
+            texto: 'Todos los campos son obligatorios',
+            tipo: 'error'
+        });
         return;
+    }
+}
+
+class Notificacion {
+    constructor({texto, tipo}){
+        this.texto = texto;
+        this.tipo = tipo;
+
+        this.mostrar();
+    }
+
+    mostrar(){
+        const alerta = document.createElement('DIV');
+        alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm');
+
+        this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500');
+
+        //elimina alerta duplicada, optional chaning (signo de interrogacion pregunta si existe)
+        const alertaPrevia = document.querySelector('.alert'); //identifica si esta la clase alert 
+        alertaPrevia?.remove();//si existe elimina la alerta previa
+
+
+        //se agrega el mensaje
+        alerta.textContent = this.texto;
+ 
+        //insertar en el dom(que inserta, antes de cual se inserta)
+        formulario.parentElement.insertBefore(alerta, formulario);
+
+        //eliminar alerta despues de 5s
+        setTimeout(() => {
+            alerta.remove();
+        }, 4000);
     }
 }
