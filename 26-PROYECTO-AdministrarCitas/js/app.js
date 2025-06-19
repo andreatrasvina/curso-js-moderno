dc5da679e109467dc4fc7e2e1a24cd897652bba6
andreatrasvina <alucerotrasvina@gmail.com>
2025-06-19 11:42:27 -0700

    211. eliminar citas

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -83,10 +83,21 @@ class AdminCitas{
         this.mostrar();
     }
 
+    eliminar(id){
+        this.citas = this.citas.filter( cita => cita.id !== id);
+        this.mostrar();
+    }
+
     mostrar(){
         //limpiar HTML previo
         while(contenedorCitas.firstChild){
             contenedorCitas.removeChild(contenedorCitas.firstChild);
+        }
+
+        //si hay citas
+        if(this.citas.length === 0){
+            contenedorCitas.innerHTML = '<p class="text-xl mt-5 mb-10 text-center">No Hay Pacientes</p>';
+            return
         }
 
         //generando las citas
@@ -126,7 +137,8 @@ class AdminCitas{
             const btnEliminar = document.createElement('button');
             btnEliminar.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
             btnEliminar.innerHTML = 'Eliminar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
-        
+            btnEliminar.onclick = () => this.eliminar(cita.id);
+
             const contenedorBtns = document.createElement('DIV');
             contenedorBtns.classList.add('flex', 'justify-between', 'mt-10');
 
