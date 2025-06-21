fef2e75868392d6931760f1097290f9cbb2a2e0f
andreatrasvina <alucerotrasvina@gmail.com>
2025-06-21 12:32:04 -0700

    224. heredar clase importada

--- 28-Modulos/js/app.js
+++ 28-Modulos/js/app.js
@@ -1,4 +1,5 @@
 import { nombreCliente, ahorro, mostrarInformacion, tieneSaldo, Cliente } from "./cliente.js";
+import { Empresa } from "./empresa.js";
 
 console.log(nombreCliente);
 console.log(ahorro);
@@ -9,3 +10,7 @@ tieneSaldo(ahorro);
 
 const cliente = new Cliente(nombreCliente, ahorro);
 console.log(cliente.mostrarInformacion());
+
+//empresa
+const empresa = new Empresa('level up access', 200000, 'E commerce');
+console.log(empresa.mostrarInformacion());
