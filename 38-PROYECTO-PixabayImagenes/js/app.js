3e6f4d8c1238da73886d5eaa255dea7d69750085
andreatrasvina <alucerotrasvina@gmail.com>
2025-07-03 10:56:44 -0700

    292. muestra las imagenes y su informacion adicional

--- 38-PROYECTO-PixabayImagenes/js/app.js
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -14,6 +14,8 @@ function validarFormulario(e){
         mostrarAlerta('error', 'Debes agregar un texto de busqueda');
         return;
     }
+
+    buscarImagenes(terminoBusqueda);
 }
 
 function mostrarAlerta(tipo, mensaje){
@@ -23,7 +25,7 @@ function mostrarAlerta(tipo, mensaje){
     if(!existe){
 
         if(tipo === 'error'){
-            
+
             const alerta = document.createElement('P');
             alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center', 'existe');
     
@@ -41,4 +43,52 @@ function mostrarAlerta(tipo, mensaje){
 
     }
 
+}
+
+function buscarImagenes(terminoBusqueda){
+    const key = '51163818-4c077900c7660cae16ff33ca2';
+    const url = `https://pixabay.com/api/?key=${key}&q=${terminoBusqueda}&per_page=100`;
+
+    fetch(url)
+        .then(consulta => consulta.json())
+        .then(imagenes => mostrarImagenes(imagenes.hits))
+}
+
+function mostrarImagenes(imagenes){
+
+    limpiarHTML();
+
+    //itera imgs y construye el html
+    imagenes.forEach(imagen => {
+        const { previewURL, likes, views, largeImageURL } = imagen;
+        console.log(imagen);
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}" />
+
+                    <div class="p-4">
+                        <p class="font-bold">${likes} <span class="font-light">Me gusta</span></p>
+                        <p class="font-bold">${views} <span class="font-light">Veces vista</span></p>
+
+                        <a 
+                        class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                        href="${largeImageURL}" target="_blank" rel="noopener noreferrer">
+                            Ver imagen
+                        </a>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+
+
+}
+
+function limpiarHTML(){
+    //borra html previo
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
 }
