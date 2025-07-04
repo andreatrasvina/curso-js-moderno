c9f4c77604442b3ab265fea03f2ac2e19b8f9545
andreatrasvina <alucerotrasvina@gmail.com>
2025-07-04 09:53:33 -0700

    300. validacion del formulario

--- 39-PROYECTO-Criptomonedas/js/app.js
+++ 39-PROYECTO-Criptomonedas/js/app.js
@@ -1,5 +1,12 @@
 
 const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+
+const objBusqueda = {
+    moneda: '',
+    criptomoneda: ''
+}
 
 //promise que resuelve al cargar las criptomonedas
 const obtenerCriptomonedas = criptomonedas => new Promise( resolve => {
@@ -9,6 +16,11 @@ const obtenerCriptomonedas = criptomonedas => new Promise( resolve => {
 
 document.addEventListener('DOMContentLoaded', () => {
     consultarCriptomonedas();
+
+    formulario.addEventListener('submit', calcularCriptomoneda);
+
+    criptomonedasSelect.addEventListener('change', leerValor);
+    monedaSelect.addEventListener('change', leerValor);
 })
 
 function consultarCriptomonedas(){
@@ -32,4 +44,43 @@ function selectCriptomonedas(criptomonedas){
 
         criptomonedasSelect.appendChild(option);
     });
+}
+
+function leerValor(e){
+    objBusqueda[e.target.name] = e.target.value;
+    //console.log(objBusqueda);
+}
+
+function calcularCriptomoneda(e){
+    e.preventDefault();
+
+    //validar form
+    const { moneda, criptomoneda } = objBusqueda;
+
+    if(moneda === '' || criptomoneda === ''){
+        mostrarAlerta('Ambos campos son obligatorios');
+        return;
+    }
+
+    //consultar api
+}
+
+function mostrarAlerta(mensaje){
+
+    const error = document.querySelector('.error');
+
+    if(!error){
+
+        const divMensaje = document.createElement('DIV');
+        divMensaje.classList.add('error');
+        divMensaje.textContent = mensaje;
+    
+        formulario.appendChild(divMensaje);
+    
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
+
+    }
+
 }
